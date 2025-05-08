b02b9e0a4b4bba9c3ed28cfa96245725f49592a7
Akpa Solomon <solomonakpas@gmail.com>
2025-05-08 10:18:19 +0100

    side and top bar

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DashboardBody from '../components/Dashboard/DashboardBody';
+
+const Dashboard = () => {
+  return (
+    <div className="flex">
+      <DashboardBody />
+    </div>
+  )
+}
+
+export default Dashboard
+
+// 
---
+++ src/routes/AllRoutes.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from '../pages/Auth/Login';
+import ForgotPassword from '../pages/Auth/ForgotPassword';
+import Otp from '../pages/Auth/Otp';
+import ResetPassword from '../pages/Auth/ResetPassword';
+import Dashboard from '../pages/Dashboard';
+
+const AllRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/otp" element={<Otp />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  )
+}
+
+export default AllRoutes
