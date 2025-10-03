25ab6966d349ce86600b0dbfa0b7a7117bb24f1b
Akpa Solomon <solomonakpas@gmail.com>
2025-10-03 08:37:27 +0100

    Update passwordReset.js

--- src/Services/passwordReset.js
+++ src/Services/passwordReset.js
@@ -2,12 +2,12 @@ import api from "./putApi";
 
 export const initiateResetPassword = async (data) => {
     const res = await api.post('/api/User/InitiateResetPassword', data);
-    console.log(res);
+    // console.log(res);
     return res.data;
 }
 
 export const resetPassword = async (data) => {
     const res = await api.post('/api/User/ResetPassword', data);
-    console.log(res);
+    // console.log(res);
     return res.data;
 }
